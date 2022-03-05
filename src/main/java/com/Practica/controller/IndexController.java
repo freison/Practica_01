63c1fdfd1934d8e124c741ce8122168b5745bb89
Freison Castro <freisoncastro@Freisons-MacBook-Pro.local>
2022-03-05 13:23:00 -0600

    DAO

--- src/main/java/com/Practica/controller/IndexController.java
+++ src/main/java/com/Practica/controller/IndexController.java
@@ -1,6 +1,22 @@
 
 package com.Practica.controller;
 
+import com.Practica.service.EstadoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
 public class IndexController {
     
+    @Autowired
+    private EstadoService estadoService;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        return "index";
+    }
 }
---
+++ src/main/java/com/Practica/dao/EstadoDao.java
@@ -0,0 +1,9 @@
+
+package com.Practica.dao;
+
+import com.Practica.domain.Estado;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EstadoDao extends CrudRepository<Estado, Long> {
+    
+}
